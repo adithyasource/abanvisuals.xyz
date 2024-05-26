7f95d3f4ed4572d9cb264a86c38ee2ba259ef42e
adithyasource <adithyasrc@gmail.com>
2024-05-26 12:53:51 +0530

    fix: issues with async fetching

--- src/routes/finance/+page.server.js
+++ src/routes/finance/+page.server.js
@@ -1,32 +1,32 @@
 import { SECRET_YOUTUBE_API } from "$env/static/private";
 
-let isLoading = true
-let videos
-
-let videosWithViews = {}
-
 export async function load() {
-  await fetch("https://api.npoint.io/7d15dcc6d912342c2681").then(async (response) => {
-    response.json().then(async (data) => {
-      videos = data;
+  let isLoading = true;
 
-      let regExp = /^.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+  let videosWithViews = {};
 
+  let videos = await fetch("https://api.npoint.io/7d15dcc6d912342c2681");
 
-      for (let key in videos) {
-        let videoId = videos[key][0].match(regExp)[1]
+  videos = await videos.json();
 
+  console.log(videos);
 
-        await fetch(
-          `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${SECRET_YOUTUBE_API}`
-        ).then((response) => {
-          response.json().then((data) => {
-            let videoViews = data.items[0].statistics.viewCount
-            videosWithViews = {...videosWithViews , [key]: [...videos[key], videoViews]}
-          });
-        });
-      }
-    });
-  });
-  return videosWithViews
+  let regExp = /^.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+
+  for (let key in videos) {
+    let videoId = videos[key][0].match(regExp)[1];
+
+    let youtubeData = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${SECRET_YOUTUBE_API}`
+    );
+
+    youtubeData = await youtubeData.json();
+
+    let videoViews = youtubeData.items[0].statistics.viewCount;
+    videosWithViews = {
+      ...videosWithViews,
+      [key]: [...videos[key], videoViews],
+    };
+  }
+  return videosWithViews;
 }
