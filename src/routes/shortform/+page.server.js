58958dd8f824f4141c2b97f58442f57bb9fd8dbe
adithyasource <adithyasrc@gmail.com>
2024-06-03 22:34:05 +0530

    feat: views can be manually added to shortform for yt and ig

---
+++ src/routes/shortform/+page.server.js
@@ -0,0 +1,40 @@
+import { SECRET_YOUTUBE_API } from "$env/static/private";
+
+export async function load() {
+  let youtubeVideosWithViews = {};
+
+  let videos = await fetch("https://api.npoint.io/8619bd1a5b4d59f26bb0");
+
+  videos = await videos.json();
+
+  let regExp = /^.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+
+  for (let key in videos) {
+    let website = videos[key][0].match(
+      /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/
+    );
+
+    if (website[0] == "www.youtube.com") {
+      let videoId = videos[key][0].match(regExp)[1];
+      console.log(videoId);
+
+      let youtubeData = await fetch(
+        `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${SECRET_YOUTUBE_API}`
+      );
+
+      youtubeData = await youtubeData.json();
+
+      let videoViews = youtubeData.items[0].statistics.viewCount;
+      youtubeVideosWithViews = {
+        ...youtubeVideosWithViews,
+        [key]: [...videos[key], videoViews],
+      };
+
+      delete videos[key];
+    }
+
+    videos = { ...videos, ...youtubeVideosWithViews };
+  }
+
+  return videos;
+}
--- src/routes/shortform/+page.svelte
+++ src/routes/shortform/+page.svelte
@@ -1,25 +1,23 @@
 <script>
   import { onMount } from "svelte";
 
-  let data;
-  let isLoading = true;
-  let videos;
-  let desktopWindow
-  let windowWidth
+  export let data;
+  $: console.log(data);
+  $: videos = Object.values(data);
+  let desktopWindow;
+  let windowWidth;
 
-  async function fetchData() {
-    fetch("https://api.npoint.io/8619bd1a5b4d59f26bb0").then((response) => {
-      response.json().then((data) => {
-        videos = data;
-
-        console.log("loaded");
-        console.log(videos);
-
-        videos = Object.values(videos);
-
-        isLoading = false;
-      });
-    });
+  function formatNumberWithCommas(number) {
+    let numberString = number.toString();
+    let parts = numberString.split(".");
+    let integerPart = parts[0];
+    let decimalPart = parts[1] || "";
+    let integerWithCommas = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    if (decimalPart !== "") {
+      return integerWithCommas + "." + decimalPart;
+    } else {
+      return integerWithCommas;
+    }
   }
 
   function onWidthChange() {
@@ -31,56 +29,70 @@
     windowWidth = window.innerWidth;
     desktopWindow = window.innerWidth > 1000;
     window.addEventListener("resize", () => {
-      onWidthChange()
+      onWidthChange();
     });
     window.addEventListener("load", () => {
-      onWidthChange()
+      onWidthChange();
     });
   });
-
-  fetchData();
 </script>
 
-{#if isLoading}
-{:else}
-  <div
-    class="w-[80vw] h-[80%] text-white flex-col gap-10 overflow-y-auto mt-14 max-[1000px]:h-[70%] max-[1000px]:mt-12"
-    >
-    <div class="text-white items-center flex flex-col gap-10">
-      <div class="videos gap-10 relative">
-        {#each videos as video}
-          <div class="group hover:z-10">
-            <div class="rounded-xl overflow-hidden relative">
-              <a href={video[0]} target="_blank">
-                <img src={video[2]} class="video w-[20vw] h-[35.56vw]" alt=""/>
-              </a>
-            </div>
-            {#if desktopWindow}
-              <div>
-                {#if video[1] != ""}
-                  <div
-                    class="cardDetails w-[20vw] bg-[#0C0C0C] -z-10 p-4 rounded-b-xl absolute max-[1000px]:relative opacity-0 translate-y-[-100%] group-hover:translate-y-[-10px] group-hover:opacity-100 group-hover:transition-all transition-opacity group-hover:duration-300">
-                    <p>{video[1]}</p>
-                  </div>
-                {/if}
-              </div>
-            {:else}
-              <div>
-                {#if video[1] != ""}
-                  <div
-                    class="cardDetails bg-[#0C0C0C] p-4 rounded-b-xl absolute max-[1000px]:relative w-[35vw] max-[1000px]:w-[75vw] translate-y-[-10px]">
-                    <p>{video[1]}</p>
-                  </div>
-                {/if}
-              </div>
-            {/if}
+<div
+  class="w-[80vw] h-[80%] text-white flex-col gap-10 overflow-y-auto mt-14 max-[1000px]:h-[70%] max-[1000px]:mt-12"
+>
+  <div class="text-white items-center flex flex-col gap-10">
+    <div class="videos gap-10 relative">
+      {#each videos as video}
+        <div class="group hover:z-10">
+          <div class="rounded-xl overflow-hidden relative">
+            <a href={video[0]} target="_blank">
+              <img src={video[2]} class="video w-[20vw] h-[35.56vw]" alt="" />
+            </a>
           </div>
-        {/each}
-        <br />
-      </div>
+          {#if desktopWindow}
+            <div>
+              {#if (parseInt(video[3]) > 100) | (video[1] != "")}
+                <div
+                  class="cardDetails w-[20vw] bg-[#0C0C0C] -z-10 p-4 rounded-b-xl absolute max-[1000px]:relative opacity-0 translate-y-[-100%] group-hover:translate-y-[-10px] group-hover:opacity-100 group-hover:transition-all transition-opacity group-hover:duration-300"
+                >
+                  <div
+                    class="font-bold text-xl absolute pt-1 pb-2 flex border bg-gradient-to-b blur-xl from-[#FF7200] to-[#FF7200] bg-clip-text text-transparent"
+                  >
+                    {#if parseInt(video[3]) > 100}
+                      {formatNumberWithCommas(video[3])} views
+                    {/if}
+                  </div>
+                  <div
+                    class="font-bold text-xl relative top-0 pt-1 pb-2 h-auto flex bg-gradient-to-b items-center from-[#FF7200] to-[#FF7200] bg-clip-text text-transparent"
+                  >
+                    {#if parseInt(video[3]) > 1000}
+                      {formatNumberWithCommas(video[3])} views
+                    {/if}
+                  </div>
+                  {#if video[1] != ""}
+                    <p>{video[1]}</p>
+                  {/if}
+                </div>
+                <br />
+              {/if}
+            </div>
+          {:else}
+            <div>
+              {#if video[1] != ""}
+                <div
+                  class="cardDetails bg-[#0C0C0C] p-4 rounded-b-xl absolute max-[1000px]:relative w-[35vw] max-[1000px]:w-[75vw] translate-y-[-10px]"
+                >
+                  <p>{video[1]}</p>
+                </div>
+              {/if}
+            </div>
+          {/if}
+        </div>
+      {/each}
+      <br />
     </div>
   </div>
-{/if}
+</div>
 
 <style scoped>
   .videos {
