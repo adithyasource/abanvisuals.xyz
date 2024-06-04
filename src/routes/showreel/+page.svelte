2666f2f522c71b64bf9775ff2fd946da9954a0e1
adithyasource <adithyasrc@gmail.com>
2024-06-04 11:21:08 +0530

    feat: showreel better styles

--- src/routes/showreel/+page.svelte
+++ src/routes/showreel/+page.svelte
@@ -11,11 +11,11 @@
 </script>
 
 <div
-  class="w-[80vw] h-[80%] text-white items-center justify-center flex flex-col gap-10 mt-14 max-[1000px]:mt-14 max-[1000px]:w-[90px]">
+  class="w-[100vw] h-[80%] text-white items-center justify-center flex flex-col gap-10 mt-14 max-[1000px]:mt-14">
   {#if website[0] == "www.youtube.com"}
     <div>
       <iframe
-        class="video w-[60vw] h-[33.75vw]"
+        class="video aspect-video w-[70vw] h-auto !rounded-xl"
         src={"https://www.youtube-nocookie.com/embed/" +
           link.match(/[?&]v=([^&]+)/)[1]}
         allowfullscreen></iframe>
@@ -26,7 +26,7 @@
     <div>
       <iframe
         src={"https://player.vimeo.com/video/" + link.match(/\/(\d+)$/)[1]}
-        class="video w-[60vw] h-[33.75vw]"
+        class="video aspect-video w-[70vw] h-auto !rounded-xl"
         frameborder="0"
         webkitallowfullscreen
         mozallowfullscreen
